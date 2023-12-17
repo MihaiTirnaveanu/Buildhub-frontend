21936826f106d15d415577cc8132be6210a67de8
MihaiTirnaveanu <mihaitirnaveanu@gmail.com>
2023-12-17 20:15:50 +0200

    Add task and link models

--- src/app/gantt/gantt.component.ts
+++ src/app/gantt/gantt.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-
+import { TaskService } from '../services/task.service';
+import { LinkService } from '../services/link.service';
 import { gantt } from 'dhtmlx-gantt';
 
 @Component({
+    providers: [TaskService, LinkService],
     encapsulation: ViewEncapsulation.None,
     selector: 'app-gantt',
     styleUrls: ['./gantt.component.css'],
@@ -12,7 +14,16 @@ import { gantt } from 'dhtmlx-gantt';
 export class GanttComponent implements OnInit {
     @ViewChild('gantt_here', { static: true }) ganttContainer!: ElementRef;
 
+    constructor(private taskService: TaskService, private linkService: LinkService){}
+
     ngOnInit(){
+        gantt.config.date_format = '%Y-%m-%d %H:%i';
+
         gantt.init(this.ganttContainer.nativeElement);
+
+        Promise.all([this.taskService.get(), this.linkService.get()])
+        .then(([data, links]) => {
+          gantt.parse({data, links});
+        });
     }
 }
---
+++ src/app/models/task.ts
@@ -0,0 +1,11 @@
+export class Task {
+    id!: number;
+    start_date!: string;
+    end_date!: string;
+    text!: string;
+    name!: string;
+    description!: string;
+    progress!: number;
+    duration!: number;
+    parent!: number;
+}
